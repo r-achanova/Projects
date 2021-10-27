5b269aa162da95fe8ab64eb11ebf5187efcb8475
r-achanova <r.achanova@pmg-pernik.com>
2021-10-27 22:14:28 +0300

    Add project ProductsDb

---
+++ DbProducts/DbProducts/Data/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using DbProducts.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbProducts.Data
+{
+  public   class ProductContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Integrated Security=true; Database=ProductsDb");
+        }
+    }
+}
---
+++ DbProducts/DbProducts/Program.cs
@@ -0,0 +1,16 @@
+﻿using DbProducts.Data;
+using DbProducts.Presentation;
+using System;
+
+namespace DbProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductContext db = new ProductContext();
+            db.Database.EnsureCreated();
+            Display display = new Display();
+        }
+    }
+}
